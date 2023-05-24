f57d2e2b2079a7322bfd25fb65f97e3656ec4182
ZainabH3 <devzhsoftware@gmail.com>
2023-05-25 01:56:10 +0300

    add sizes

---
+++ lib/core/resources/manager-sizes.dart
@@ -0,0 +1,39 @@
+import '../util/size-util.dart';
+
+class ManagerHeight {
+  static double h2 = SizeUtil.setHeight(2);
+  static double h4 = SizeUtil.setHeight(4);
+  static double h6 = SizeUtil.setHeight(6);
+  static double h8 = SizeUtil.setHeight(8);
+  static double h10 = SizeUtil.setHeight(10);
+}
+
+class ManagerWidth {
+  static double w2 = SizeUtil.setWidth(2);
+  static double w4 = SizeUtil.setWidth(4);
+  static double w6 = SizeUtil.setWidth(6);
+  static double w8 = SizeUtil.setWidth(8);
+  static double w10 = SizeUtil.setWidth(10);
+}
+
+class ManagerRadius {
+  static double r2 = SizeUtil.setRadius(2);
+  static double r4 = SizeUtil.setRadius(4);
+  static double r6 = SizeUtil.setRadius(6);
+  static double r8 = SizeUtil.setRadius(8);
+  static double r10 = SizeUtil.setRadius(10);
+}
+
+class ManagerIconSize {
+  static double s2 = 2;
+  static double s4 = 4;
+  static double s6 = 6;
+  static double s8 = 8;
+  static double s10 = 10;
+}
+
+class ManagerOpacity {
+  static double op0_2 = 0.2;
+  static double op0_4 = 0.4;
+  static double op0_6 = 0.6;
+}
