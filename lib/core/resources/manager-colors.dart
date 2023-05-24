77096d1e23a8c9f7991486f779e0a2b3888065a9
ZainabH3 <devzhsoftware@gmail.com>
2023-05-25 01:56:00 +0300

    add colors

---
+++ lib/core/resources/manager-colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors{
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color primaryColor1 = Color(0xFF602BA3);
+  static const Color primaryColor2 = Color(0xFF8B3DEE);
+  static const Color primaryColor3 = Color(0xFFE0C8FE);
+  static const Color semanticColor1 = Color(0xFF00C48C);
+  static const Color semanticColor2 = Color(0xFF7DDFC3);
+  static const Color semanticColor3 = Color(0xFFD5F2EA);
+  static const Color textColor1 = Color(0xFF24103E);
+  static const Color textColor2 = Color(0xFF52465F);
+  static const Color textColor3 = Color(0xFFC2B6CF);
+  static const Color textColor4 = Color(0xFFEAE9F7);
+  static const Color textColor5 = Color(0xFFF7F6FB);
+  static const Color textColor6 = Color(0xFFFFFFFF);
+  static const Color gradientColor1 = Color(0xFF602BA3);
+  static const Color gradientColor2 = Color(0xFFFF9300);
+  static const Color gradientColor3 = Colors.grey;
+  static const Color accentColor1 = Colors.orange;
+  static const Color accentColor2 = Color(0xFFFFC16D);
+  static const Color accentColor3 = Color(0xFFFFE8C8);
+  static const Color accentColor4 = Color(0xFFFF5244);
+  static const Color accentColor5 = Color(0xFFFF847A);
+  static const Color accentColor6 = Color(0xFFFFBCB7);
+
+
+}
