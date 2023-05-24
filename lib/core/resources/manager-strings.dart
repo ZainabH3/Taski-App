3093dc3dc300a8726c67a92b56ee1bdf43b3187d
ZainabH3 <devzhsoftware@gmail.com>
2023-05-25 01:56:20 +0300

    add strings

---
+++ lib/core/resources/manager-strings.dart
@@ -0,0 +1,6 @@
+class ManagerStrings{
+  static const String noRouteFound = 'No Route Found';
+  static const String logoutFailed = 'Logout Failed';
+  static const String noRoutFound = 'No Rout Found';
+  static const String skip = 'Skip';
+}
