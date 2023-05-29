df25294c97b60863fb27facd7d1130b7b024ebb7
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 00:27:51 +0300

    add dependency-injection.dart

--- lib/config/dependency-injection.dart
+++ lib/config/dependency-injection.dart
@@ -1,6 +1,19 @@
 
+import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
+
+import '../fetaures/splash/presentation/controller/splash-controller.dart';
+
 
 final instance = GetIt.instance;
 
-initModules(){}
+void initModules() {
+  initSplash() {
+    Get.put<SplashController>(SplashController());
+  }
+
+  disposeSplash() {
+    Get.delete<SplashController>();
+  }
+}
+
