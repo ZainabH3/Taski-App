30f862cb46e8c9e91f7c9090a469697bf621007a
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 00:22:36 +0300

    edite

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,16 @@
 class Constants {
   static const String baseurl = 'http:// example.com';
+  static const double deviceWidth = 375;
+  static const double deviceHeight = 812;
+  static const int  splashDuration = 3;
+  static const int  outBoardingDurationTime = 1;
+  static const double elevationAppBar = 0;
+  static const double elevationButton = 0;
+  static const int sliderItems = 3;
+}
+
+class ConstantsPrefsKeys {
+  static const String outBoardingViewedKey = 'out_boarding_viewed';
+
+
 }
