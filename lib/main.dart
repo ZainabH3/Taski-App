709cfd821b2ff3ab0a5ead0cb8123f80d5332a46
ZainabH3 <devzhsoftware@gmail.com>
2023-05-30 00:23:33 +0300

    edite

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taski_app/core/service/theme-service.dart';
+import 'package:taski_app/routes/routes.dart';
+
+import 'config/constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final ThemeService _themeService;
+   MyApp({super.key}) : _themeService = ThemeService();
 
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return ScreenUtilInit(
+        splitScreenMode: true,
+        minTextAdapt: true,
+         designSize: const Size(
+           Constants.deviceWidth,
+           Constants.deviceHeight,
+         ),
 
+      builder: (context,child) {
+        return MaterialApp(
 
+ debugShowCheckedModeBanner: false,
+          initialRoute: Routes.splashView,
+          onGenerateRoute: RouteGenerator.getRoute,
+          themeMode:_themeService.getThemeMode() ,
+          //themeMode:_themeService.getThemeData() ,
+
+        );
+      }
     );
   }
 }
