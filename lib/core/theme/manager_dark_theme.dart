663914c80c9ac3386d6902c10ffc2eeb412d1af2
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:46:28 +0300

    create manager dark theme

---
+++ lib/core/theme/manager_dark_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:taski_app/core/resources/manager_text_theme_light.dart';
+
+import '../../config/constants.dart';
+import '../resources/manager-colors.dart';
+import '../resources/manager-fonts.dart';
+import '../resources/manager-styles.dart';
+
+
+ThemeData managerDarkThemeData() {
+  return ThemeData.dark().copyWith(
+    useMaterial3: true,
+
+    /// MAIN COLORS APP
+    primaryColor: ManagerColors.primaryColor1,
+    primaryColorLight: ManagerColors.primaryColor1,
+
+    // this for disabled button colors
+    // disabledColor: ManagerColors.disabledColor,
+    splashColor: ManagerColors.greyLight,
+    scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorDark,
+
+    /// APP BAR THEME
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ManagerColors.greyLight,
+      elevation: Constants.elevationAppBar,
+      titleTextStyle: getBoldTextStyle(
+        fontSize: ManagerFontSize.s14,
+        color: ManagerColors.primaryColor1,
+      ),
+    ),
+
+    /// BUTTON THEME
+    buttonTheme: const ButtonThemeData(
+      shape: RoundedRectangleBorder(),
+      disabledColor: ManagerColors.greyLight,
+      // buttonColor: ManagerColors.buttonColorDarK,
+    ),
+
+    textTheme: ManagerTextThemeDark(),
+  );
+}
