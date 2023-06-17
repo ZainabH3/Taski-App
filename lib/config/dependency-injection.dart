6b78a8584e52358f2ada5dc44a972cde89bace20
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:43:58 +0300

    edite

--- lib/config/dependency-injection.dart
+++ lib/config/dependency-injection.dart
@@ -1,6 +1,6 @@
-
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
+import 'package:taski_app/fetaures/out-boarding/presentation/controller/out_boarding_controller.dart';
 
 import '../fetaures/splash/presentation/controller/splash-controller.dart';
 
@@ -8,12 +8,22 @@ import '../fetaures/splash/presentation/controller/splash-controller.dart';
 final instance = GetIt.instance;
 
 void initModules() {
-  initSplash() {
-    Get.put<SplashController>(SplashController());
-  }
 
-  disposeSplash() {
-    Get.delete<SplashController>();
-  }
 }
+
+initSplash() {
+  Get.put<SplashController>(SplashController());
+}
+
+void initOutBoarding() {
+  disposeSplash();
+  Get.put<OutBoardingController>(OutBoardingController());
+}
+
+void disposeSplash() {
+  Get.delete<SplashController>();
+}
+
+
+
 
