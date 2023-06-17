5fc6faa6f043b9479b0face569b6f4f43d99e2e9
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:45:33 +0300

    add strings

--- lib/core/resources/manager-strings.dart
+++ lib/core/resources/manager-strings.dart
@@ -3,4 +3,12 @@ class ManagerStrings{
   static const String logoutFailed = 'Logout Failed';
   static const String noRoutFound = 'No Rout Found';
   static const String skip = 'Skip';
+  static const String outBoardingSubTitle1 = 'Hello!';
+  static const String outBoardingSubTitle2 = 'Solving';
+  static const String outBoardingSubTitle3 = 'Arrangement';
+   static const String outBoardingTitle1 = 'Welcome!!! Do you want clear task super fast with Taski';
+   static const String outBoardingTitle2= 'Easily arrange work order for you to easily manage';
+   static const String outBoardingTitle3 = 'It has never been easier to complete tasks. Get started with us!';
+
+
 }
