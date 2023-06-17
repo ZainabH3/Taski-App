5b2b12e58dedc9a1ac747367cb995eab3df029b6
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:48:00 +0300

    edite routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,23 +1,41 @@
 
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../config/dependency-injection.dart';
+import '../core/resources/manager-strings.dart';
+import '../fetaures/out-boarding/presentation/view/out_boarding_view.dart';
 import '../fetaures/splash/presentation/view/splash-view.dart';
 
 class Routes {
   static const String splashView = '/splash_view';
+  static const String outBoardingView = '/out_boarding_view';
 }
 
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.splashView:
-
-        return MaterialPageRoute(builder: (_) =>SplashView() );
+        initSplash();
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.outBoardingView:
+        return MaterialPageRoute(builder: (_) => const OutBoardingView());
       default:
-        return MaterialPageRoute(builder: (_) => SplashView());
+        return unDefinedRoute();
     }
   }
 
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title:  const Text(ManagerStrings.noRouteFound),
+        ),
+        body: const Center(
+          child: Text(ManagerStrings.noRouteFound),
+        ),
+      ),
+    );
+  }
 }
-
