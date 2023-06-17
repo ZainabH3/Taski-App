7d70250962452111345d44848214a0623a3924b2
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:45:08 +0300

    add fonts

--- lib/core/resources/manager-fonts.dart
+++ lib/core/resources/manager-fonts.dart
@@ -21,4 +21,6 @@ class ManagerFontSize {
   static  double s10 = SizeUtil.setFontSize(10.0);
   static  double s16 = SizeUtil.setFontSize(16.0);
   static  double s20 = SizeUtil.setFontSize(20.0);
+  static  double s14 = SizeUtil.setFontSize(14.0);
+  static  double s34 = SizeUtil.setFontSize(34.0);
 }
