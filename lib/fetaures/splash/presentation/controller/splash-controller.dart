469132b736f0899388f78d8d32d23f941170abd0
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:48:18 +0300

    edite

--- lib/fetaures/splash/presentation/controller/splash-controller.dart
+++ lib/fetaures/splash/presentation/controller/splash-controller.dart
@@ -1,18 +1,17 @@
 import 'package:get/get.dart';
-
 import '../../../../config/constants.dart';
+import '../../../../routes/routes.dart';
 
-class SplashController extends GetxController{
+class SplashController extends GetxController {
 
   @override
   void onInit() {
     super.onInit();
     Future.delayed(
-        const Duration(
-          seconds: Constants.splashDuration,
-        ), () {
-
-      }
+      const Duration(seconds: Constants.splashDuration),
+          () {
+        Get.offAllNamed(Routes.outBoardingView);
+      },
     );
   }
 }
