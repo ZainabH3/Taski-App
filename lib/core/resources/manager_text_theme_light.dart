b8f6f0361220229a5bd1312395abc9816a55d44b
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:47:08 +0300

    create manager text theme light

---
+++ lib/core/resources/manager_text_theme_light.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'manager-colors.dart';
+import 'manager-fonts.dart';
+import 'manager-styles.dart';
+
+class ManagerTextThemeLight extends TextTheme {
+  @override
+  TextStyle get displayMedium => getMediumTextStyle(
+    fontSize: ManagerFontSize.s20,
+    color: ManagerColors.textColorbig,
+  );
+  @override
+  TextStyle get displaySmall => getBoldTextStyle(
+    fontSize: ManagerFontSize.s16,
+    color: ManagerColors.textColor3,
+  );
+
+  @override
+  TextStyle get headlineMedium => getMediumTextStyle(
+    fontSize: ManagerFontSize.s16,
+    color: ManagerColors.textColorbig,
+  );
+
+  @override
+  TextStyle get titleMedium => getMediumTextStyle(
+    fontSize: ManagerFontSize.s14,
+    color: ManagerColors.textColorbig,
+  );
+
+  @override
+  TextStyle get bodyLarge => getRegularTextStyle(
+    fontSize: ManagerFontSize.s16,
+    color: ManagerColors.textColorbig,
+  );
+
+}
