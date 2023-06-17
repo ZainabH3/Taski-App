54b2744cdb5716a9500d616d2412009b2c33eee0
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:47:40 +0300

    create out boarding controller

---
+++ lib/fetaures/out-boarding/presentation/controller/out_boarding_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class OutBoardingController extends GetxController {
+  late PageController pageController;
+  static const firstPage = 0;
+  static const lastPage = 2;
+  int currentPage = firstPage;
+
+}
+
