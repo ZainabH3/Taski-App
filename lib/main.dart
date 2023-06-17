6f28c328ca029774a7c77aa5601ef077b91fd41c
ZainabH3 <devzhsoftware@gmail.com>
2023-06-18 02:44:32 +0300

    edite

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:taski_app/core/service/theme-service.dart';
 import 'package:taski_app/routes/routes.dart';
-
 import 'config/constants.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   final ThemeService _themeService;
-   MyApp({super.key}) : _themeService = ThemeService();
 
+  MyApp({super.key}) : _themeService = ThemeService();
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         splitScreenMode: true,
         minTextAdapt: true,
-         designSize: const Size(
-           Constants.deviceWidth,
-           Constants.deviceHeight,
-         ),
-
-      builder: (context,child) {
-        return  MaterialApp(
-
- debugShowCheckedModeBanner: false,
-          initialRoute:   Routes.splashView,
-          onGenerateRoute: RouteGenerator.getRoute,
-          themeMode:_themeService.getThemeMode() ,
-          theme:_themeService.getThemeData() ,
-
-        );
-      }
-    );
+        designSize: const Size(
+          Constants.deviceWidth,
+          Constants.deviceHeight,
+        ),
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: Routes.splashView,
+            onGenerateRoute: RouteGenerator.getRoute,
+            themeMode: _themeService.getThemeMode(),
+            theme: _themeService.getThemeData(),
+          );
+        });
   }
 }
-
