a49aa4668e07b9d9b1e461657cf5640f8ae6f9ff
ZainabH3 <devzhsoftware@gmail.com>
2023-06-19 06:15:16 +0300

    edit assets

--- lib/core/resources/manager-assets.dart
+++ lib/core/resources/manager-assets.dart
@@ -6,26 +6,10 @@ class ManagerAssets{
  static const String logo ='$imagesPath/ logo.png';
  static const String vector_png ='$imagesPath/vector_png.png';
  static const String outBoardingIllustration1 = '$imagesPath/illustration1.png';
- static const String outBoardingIllustration2 = '$imagesPath/illustration2.svg';
- static const String outBoardingIllustration43 = '$imagesPath/illustration3.svg';
+ static const String outBoardingIllustration2 = '$imagesPath/illustration2.png';
+ static const String outBoardingIllustration3 = '$imagesPath/illustration3.png';
 
- // static const String vector = '$imagesPath/vector.svg';
- // static const String vector1 = '$imagesPath/vector1.png';
- // static const String vector2 = '$imagesPath/vector2.png';
- // static const String vector3 = '$imagesPath/vector3.png';
- // static const String vector4 = '$imagesPath/vector4.png';
- // static const String vector5 = '$imagesPath/vector5.png';
- // static const String vector6 = '$imagesPath/vector6.png';
- // static const String vector7 = '$imagesPath/vector7.png';
- // static const String vector8 = '$imagesPath/vector8.png';
- // static const String vector9 = '$imagesPath/vector9.png';
- // static const String vector10 = '$imagesPath/vector10.png';
- // static const String vector11 = '$imagesPath/vector11.png';
- // static const String vector12 = '$imagesPath/vector12.png';
- // static const String vector13 = '$imagesPath/vector13.png';
- // static const String vector14 = '$imagesPath/vector14.png';
- // static const String vector15 = '$imagesPath/vector15.png';
- // static const String vector16 = '$imagesPath/vector16.svg';
+
 
 }
 
