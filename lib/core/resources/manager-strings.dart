1e496de86e1ed2ee2c4b4ceee607822bd6bf2196
ZainabH3 <devzhsoftware@gmail.com>
2023-06-20 00:04:01 +0300

    add strings

--- lib/core/resources/manager-strings.dart
+++ lib/core/resources/manager-strings.dart
@@ -10,6 +10,7 @@ class ManagerStrings{
    static const String outBoardingSubTitle1 = 'Welcome!!! Do you want clear task super fast with Taski';
    static const String outBoardingSubTitle2= 'Easily arrange work order for you to easily manage';
    static const String outBoardingSubTitle3 = 'It has never been easier to complete tasks. Get started with us!';
+  static const String getStartedButton = 'GET STARTED';
 
 
 }
