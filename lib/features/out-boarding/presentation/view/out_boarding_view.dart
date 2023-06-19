074be4d10fe34502c3c72b17a88e79a45b607725
ZainabH3 <devzhsoftware@gmail.com>
2023-06-20 00:04:58 +0300

    edit out boarding view

--- lib/features/out-boarding/presentation/view/out_boarding_view.dart
+++ lib/features/out-boarding/presentation/view/out_boarding_view.dart
@@ -18,97 +18,147 @@ class OutBoardingView extends StatelessWidget {
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.symmetric(
-            horizontal: ManagerWidth.w16,
+            horizontal: ManagerWidth.w20,
             vertical: ManagerHeight.h20,
           ),
           child: GetBuilder<OutBoardingController>(builder: (controller) {
             return Column(
               children: [
-                Align(
-                  alignment: AlignmentDirectional.centerEnd,
-                  child: mainButton(
-                    onPressed: () {
-                      controller.nextPage();
-                    },
-                    child: Text(
-                      ManagerStrings.skip,
-                      style: getRegularTextStyle(
-                        fontSize: ManagerFontSize.s16,
-                        color: ManagerColors.gradientColor2,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Visibility(
+                      visible: controller.showBackButton(),
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          mainButton(
+                            minWidth: 30,
+                            onPressed: () {
+                              controller.nextPage();
+                            },
+                            child: Container(
+                              width: ManagerWidth.w50,
+                              height: ManagerHeight.h50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                color: ManagerColors.accentColor1,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.arrow_back_outlined,
+                                  color: ManagerColors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
+
+                    Visibility(
+                      visible: controller.isNotLastedPage(),
+                      child: Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: mainButton(
+                          onPressed: () {
+                            controller.nextPage();
+                          },
+                          child: Text(
+                            ManagerStrings.skip,
+                            style: getRegularTextStyle(
+                              fontSize: ManagerFontSize.s16,
+                              color: ManagerColors.gradientColor2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: ManagerHeight.h48),
+                const Spacer(flex: 1),
                 Expanded(
+                  flex: 30,
                   child: PageView(
                     controller: controller.pageController,
                     children: [
-                      ...controller.PageViewItems,
+                      ...controller.pageViewItems,
                     ],
                     onPageChanged: (index) {
                       controller.setCurrentPage(index);
                     },
                   ),
                 ),
-                SizedBox(height: ManagerHeight.h48),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(3, (index) {
-                    final bool isPrimary = index == 0;
-                    return Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 5),
-                      width: isPrimary ? 25 : 6,
-                      height: 6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: isPrimary
-                            ? ManagerColors.primaryColor1
-                            : ManagerColors.greyLight,
-                      ),
-                    );
-                  }),
-                ),
-                SizedBox(height: ManagerHeight.h26),
-                mainButton(
-                  onPressed: () {
-                    controller.nextPage();
-                  },
+                const Spacer(flex: 1),
+                SizedBox(
+                  height: ManagerHeight.h6,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Container(
-                            width: ManagerWidth.w80,
-                            height: ManagerHeight.h80,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: ManagerColors.greyLight,
-                                width: ManagerWidth.w1,
-                              ),
-                            ),
+                    children:
+                        List.generate(controller.pageViewItems.length, (index) {
+                      return GetBuilder<OutBoardingController>(
+                          builder: (controller) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          width: controller.isFirstedPage(index) ? 25 : 6,
+                          height: 6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: controller.currentPage == index
+                                ? ManagerColors.primaryColor1
+                                : ManagerColors.greyLight,
                           ),
-                          Container(
-                            width: ManagerWidth.w50,
-                            height: ManagerHeight.h50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: ManagerColors.accentColor1,
-                            ),
-                            child: const Center(
-                              child: Icon(
-                                Icons.arrow_forward,
-                                color: ManagerColors.white,
-                              ),
-                            ),
-                          ),
-                        ],
+                        );
+                      });
+                    }),
+                  ),
+                ),
+                SizedBox(height: ManagerHeight.h26),
+                Visibility(
+                  visible: controller.isNotLastedPage(),
+                  replacement: mainButton(
+                    child: Text(
+                      ManagerStrings.getStartedButton,
+                      style: getBoldTextStyle(
+                          fontSize: ManagerFontSize.s16,
+                          color: ManagerColors.white,
                       ),
-
-                    ],
+                    ),
+                    minWidth: double.infinity,
+                    height: ManagerHeight.h48,
+                    color: ManagerColors.gradientColor2,
+                  ),
+                  child: mainButton(
+                    minWidth: 30,
+                    onPressed: () {
+                      controller.nextPage();
+                    },
+                    child: Container(
+                      width: ManagerWidth.w80,
+                      height: ManagerHeight.h80,
+                      padding: EdgeInsets.symmetric(
+                        vertical: ManagerHeight.h14,
+                        horizontal: ManagerWidth.w14,
+                      ),
+                      decoration: BoxDecoration(
+                        color: ManagerColors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: ManagerColors.greyLight,
+                          width: ManagerWidth.w1,
+                        ),
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: ManagerColors.accentColor1,
+                        ),
+                        child: Center(
+                          child: Text('${controller.currentPage + 1}'),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
