741e11558eb45aafbd4981950280d37e48cae506
ZainabH3 <devzhsoftware@gmail.com>
2023-06-19 06:13:52 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -6,6 +6,8 @@ class Constants {
   static const int  outBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
   static const double elevationButton = 0;
+  static const firstPage = 0;
+  static const lastPage = 2;
   static const int sliderItems = 3;
   static const String outBoardingViewedKey = 'out_boarding_viewed';
 }
