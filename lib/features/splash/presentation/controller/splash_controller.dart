4597e98442327f16585bc458fd54cba92beb231c
ZainabH3 <devzhsoftware@gmail.com>
2023-06-24 17:00:15 +0300

    edit splash controller

--- lib/features/splash/presentation/controller/splash_controller.dart
+++ lib/features/splash/presentation/controller/splash_controller.dart
@@ -1,15 +1,24 @@
 import 'package:get/get.dart';
+import 'package:taski_app/config/dependency-injection.dart';
+import 'package:taski_app/core/storage/local/app_settings_shared_preferences.dart';
 import '../../../../config/constants.dart';
 import '../../../../routes/routes.dart';
 
 class SplashController extends GetxController {
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
+  instance<AppSettingsSharedPreferences>();
+
   @override
   void onInit() {
     super.onInit();
     Future.delayed(
       const Duration(seconds: Constants.splashDuration),
-      () {
-        Get.offAllNamed(Routes.outBoardingView);
+          () {
+        if (_appSettingsSharedPreferences.getOutBoardingViewed()) {
+          Get.offAllNamed(Routes.loginView);
+        } else {
+          Get.offAllNamed(Routes.outBoardingView);
+        }
       },
     );
   }
