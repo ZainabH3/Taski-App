fd255ddd2533988565935d133508ef4645aa365b
ZainabH3 <devzhsoftware@gmail.com>
2023-06-24 16:57:13 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,14 +2,15 @@ class Constants {
   static const String baseurl = 'http:// example.com';
   static const double deviceWidth = 375;
   static const double deviceHeight = 812;
-  static const int  splashDuration = 3;
-  static const int  outBoardingDurationTime = 1;
+  static const int splashDuration = 3;
+  static const int outBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
   static const double elevationButton = 0;
   static const firstPage = 0;
   static const lastPage = 2;
   static const int sliderItems = 3;
-  static const String outBoardingViewedKey = 'out_boarding_viewed';
 }
 
-
+class ConstantsPrefsKeys {
+  static const String outBoardingViewedKey = 'out_boarding_viewed';
+}
