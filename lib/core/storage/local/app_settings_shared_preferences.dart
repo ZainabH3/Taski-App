d155cec20c6fb2e861418e2a7a6d9008f021b950
ZainabH3 <devzhsoftware@gmail.com>
2023-06-24 15:36:33 +0300

    create app_settings_shared_preferences

--- lib/core/storage/local/app_settings_shared_preferences.dart
+++ lib/core/storage/local/app_settings_shared_preferences.dart
@@ -3,5 +3,4 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AppSettingsSharedPreferences {
   final SharedPreferences _sharedPreferences;
 
-  AppSettingsSharedPreferences(this._sharedPreferences);
-}
+  AppSettingsSharedPreferences(this._sharedPreferences);}
