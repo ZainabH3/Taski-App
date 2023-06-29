f6e809fcd2bff5f6568ae6a8c5aca30ea40d4f24
ZainabH3 <devzhsoftware@gmail.com>
2023-06-29 22:58:33 +0300

    add strings

--- lib/core/resources/manager-strings.dart
+++ lib/core/resources/manager-strings.dart
@@ -1,4 +1,4 @@
-class ManagerStrings{
+class ManagerStrings {
   static const String noRouteFound = 'No Route Found';
   static const String logoutFailed = 'Logout Failed';
   static const String noRoutFound = 'No Rout Found';
@@ -7,13 +7,15 @@ class ManagerStrings{
   static const String outBoardingTitle1 = 'Hello!';
   static const String outBoardingTitle2 = 'Solving';
   static const String outBoardingTitle3 = 'Arrangement';
-   static const String outBoardingSubTitle1 = 'Welcome!!! Do you want clear task super fast with Taski';
-   static const String outBoardingSubTitle2= 'Easily arrange work order for you to easily manage';
-   static const String outBoardingSubTitle3 = 'It has never been easier to complete tasks. Get started with us!';
+  static const String outBoardingSubTitle1 =
+      'Welcome!!! Do you want clear task super fast with Taski';
+  static const String outBoardingSubTitle2 =
+      'Easily arrange work order for you to easily manage';
+  static const String outBoardingSubTitle3 =
+      'It has never been easier to complete tasks. Get started with us!';
   static const String getStartedButton = 'GET STARTED';
   static const String bearer = 'Bearer';
   static const String error = 'error';
-  static const String badRequest = ' BadRequest';
-
-
+  static const String badRequest = ' Bad Request';
+  static const String noInternetConnection = ' No Internet Connection';
 }
